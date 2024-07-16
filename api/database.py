327655e81fedaac3418a94064a38e8e3dbf655b4
lemon-lime-honey <bs0308@g.skku.edu>
2024-07-16 09:40:22 +0900

    feat: :card_file_box: update database settings
    - 데이터베이스 관련 설정 갱신

--- api/database.py
+++ api/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.environ.get("DB_URL")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
