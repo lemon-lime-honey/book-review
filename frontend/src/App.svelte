30732db5aa76cba5e77cf7719e06a949561322a9
lemon-lime-honey <bs0308@g.skku.edu>
2024-07-16 19:03:52 +0900

    feat: :sparkles: add temporary main page
    - 임시 더미 메인 페이지 추가

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,9 +1,11 @@
 <script>
   import Router from 'svelte-spa-router';
+  import Index from './routes/Index.svelte';
   import AccountCreate from './routes/AccountCreate.svelte';
   import AccountLogin from './routes/AccountLogin.svelte';
 
   const routes = {
+    '/': Index,
     '/account-create': AccountCreate,
     '/account-login': AccountLogin,
   };
