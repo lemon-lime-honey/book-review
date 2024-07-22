16c7e0ba58f12a0dee499cb88fb09893cd1634e3
lemon-lime-honey <bs0308@g.skku.edu>
2024-07-22 12:39:33 +0900

    feat: :card_file_box: add like and dislike
    - 좋아요, 싫어요 기능을 위한 필드 추가

--- api/models.py
+++ api/models.py
@@ -1,8 +1,37 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from datetime import datetime
-from typing import List, Optional
+from typing import List, Optional, Set
 from database import Base
+
+
+like_review_table = sa.Table(
+    "like_review_table",
+    Base.metadata,
+    sa.Column("like_review_account", sa.ForeignKey("account.id"), primary_key=True),
+    sa.Column("like_review", sa.ForeignKey("review.id"), primary_key=True),
+)
+
+dislike_review_table = sa.Table(
+    "dislike_review_table",
+    Base.metadata,
+    sa.Column("dislike_review_account", sa.ForeignKey("account.id"), primary_key=True),
+    sa.Column("dislike_review", sa.ForeignKey("review.id"), primary_key=True),
+)
+
+like_comment_table = sa.Table(
+    "like_comment_table",
+    Base.metadata,
+    sa.Column("like_comment_account", sa.ForeignKey("account.id"), primary_key=True),
+    sa.Column("like_comment", sa.ForeignKey("comment.id"), primary_key=True),
+)
+
+dislike_comment_table = sa.Table(
+    "dislike_comment_table",
+    Base.metadata,
+    sa.Column("dislike_comment_account", sa.ForeignKey("account.id"), primary_key=True),
+    sa.Column("dislike_comment", sa.ForeignKey("comment.id"), primary_key=True),
+)
 
 
 class Account(Base):
@@ -15,8 +44,24 @@ class Account(Base):
     password: so.Mapped[str] = so.mapped_column(sa.String, nullable=False)
     email: so.Mapped[str] = so.mapped_column(sa.String, unique=True, nullable=False)
 
-    reviews: so.Mapped[List["Review"]] = so.relationship(back_populates="author", passive_deletes=True)
-    comments: so.Mapped[List["Comment"]] = so.relationship(back_populates="author", passive_deletes=True)
+    reviews: so.Mapped[List["Review"]] = so.relationship(
+        back_populates="author", passive_deletes=True
+    )
+    comments: so.Mapped[List["Comment"]] = so.relationship(
+        back_populates="author", passive_deletes=True
+    )
+    like_review: so.Mapped[Set["Review"]] = so.relationship(
+        secondary=like_review_table, back_populates="like_accounts"
+    )
+    dislike_review: so.Mapped[Set["Review"]] = so.relationship(
+        secondary=dislike_review_table, back_populates="dislike_accounts"
+    )
+    like_comment: so.Mapped[Set["Comment"]] = so.relationship(
+        secondary=like_comment_table, back_populates="like_accounts"
+    )
+    dislike_comment: so.Mapped[Set["Comment"]] = so.relationship(
+        secondary=dislike_comment_table, back_populates="dislike_accounts"
+    )
 
 
 class Review(Base):
@@ -27,23 +72,33 @@ class Review(Base):
     subject: so.Mapped[str] = so.mapped_column(sa.String(30), nullable=False)
     content: so.Mapped[str] = so.mapped_column(sa.Text, nullable=False)
     created_at: so.Mapped[datetime] = so.mapped_column(sa.DateTime, nullable=False)
-    updated_at: so.Mapped[Optional[datetime]] = so.mapped_column(sa.DateTime, nullable=True)
+    updated_at: so.Mapped[Optional[datetime]] = so.mapped_column(
+        sa.DateTime, nullable=True
+    )
 
     author_id: so.Mapped[int] = so.mapped_column(
         sa.ForeignKey("account.id"), nullable=False
     )
     author: so.Mapped["Account"] = so.relationship(back_populates="reviews")
-    comments: so.Mapped[List["Comment"]] = so.relationship(back_populates="review", passive_deletes=True)
+    comments: so.Mapped[List["Comment"]] = so.relationship(
+        back_populates="review", passive_deletes=True
+    )
+    like_accounts: so.Mapped[Set["Account"]] = so.relationship(
+        secondary=like_review_table, back_populates="like_reviews"
+    )
+    dislike_accounts: so.Mapped[Set["Account"]] = so.relationship(
+        secondary=dislike_review_table, back_populates="dislike_reviews"
+    )
 
 
 class Comment(Base):
     __tablename__ = "comment"
-    
+
     id: so.Mapped[int] = so.mapped_column(sa.Integer, primary_key=True)
     content: so.Mapped[str] = so.mapped_column(sa.String(300), nullable=False)
     created_at: so.Mapped[datetime] = so.mapped_column(sa.DateTime, nullable=False)
     updated_at: so.Mapped[datetime] = so.mapped_column(sa.DateTime, nullable=True)
-    
+
     review_id: so.Mapped[int] = so.mapped_column(
         sa.ForeignKey("review.id"), nullable=False
     )
@@ -52,3 +107,9 @@ class Comment(Base):
         sa.ForeignKey("account.id"), nullable=False
     )
     author: so.Mapped["Account"] = so.relationship(back_populates="comments")
+    like_accounts: so.Mapped[Set["Account"]] = so.relationship(
+        secondary=like_comment_table, back_populates="like_comments"
+    )
+    dislike_accounts: so.Mapped[Set["Account"]] = so.relationship(
+        secondary=dislike_comment_table, back_populates="dislike_comments"
+    )
