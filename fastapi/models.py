27c819bd256a867fdbce6ec98dffdf3dea06b255
lemon-lime-honey <bs0308@g.skku.edu>
2024-07-28 15:52:17 +0900

    fix: :card_file_box: fix cascade settings
    - cascade 옵션 수정

--- fastapi/models.py
+++ fastapi/models.py
@@ -8,36 +8,76 @@ from database import Base
 like_review_table = sa.Table(
     "like_review_table",
     Base.metadata,
-    sa.Column("like_review_account", sa.ForeignKey("account.id"), primary_key=True),
-    sa.Column("like_review", sa.ForeignKey("review.id"), primary_key=True),
+    sa.Column(
+        "like_review_account",
+        sa.ForeignKey("account.id", ondelete="CASCADE"),
+        primary_key=True,
+    ),
+    sa.Column(
+        "like_review", sa.ForeignKey("review.id", ondelete="CASCADE"), primary_key=True
+    ),
 )
 
 dislike_review_table = sa.Table(
     "dislike_review_table",
     Base.metadata,
-    sa.Column("dislike_review_account", sa.ForeignKey("account.id"), primary_key=True),
-    sa.Column("dislike_review", sa.ForeignKey("review.id"), primary_key=True),
+    sa.Column(
+        "dislike_review_account",
+        sa.ForeignKey("account.id", ondelete="CASCADE"),
+        primary_key=True,
+    ),
+    sa.Column(
+        "dislike_review",
+        sa.ForeignKey("review.id", ondelete="CASCADE"),
+        primary_key=True,
+    ),
 )
 
 like_comment_table = sa.Table(
     "like_comment_table",
     Base.metadata,
-    sa.Column("like_comment_account", sa.ForeignKey("account.id"), primary_key=True),
-    sa.Column("like_comment", sa.ForeignKey("comment.id"), primary_key=True),
+    sa.Column(
+        "like_comment_account",
+        sa.ForeignKey("account.id", ondelete="CASCADE"),
+        primary_key=True,
+    ),
+    sa.Column(
+        "like_comment",
+        sa.ForeignKey("comment.id", ondelete="CASCADE"),
+        primary_key=True,
+    ),
 )
 
 dislike_comment_table = sa.Table(
     "dislike_comment_table",
     Base.metadata,
-    sa.Column("dislike_comment_account", sa.ForeignKey("account.id"), primary_key=True),
-    sa.Column("dislike_comment", sa.ForeignKey("comment.id"), primary_key=True),
+    sa.Column(
+        "dislike_comment_account",
+        sa.ForeignKey("account.id", ondelete="CASCADE"),
+        primary_key=True,
+    ),
+    sa.Column(
+        "dislike_comment",
+        sa.ForeignKey("comment.id", ondelete="CASCADE"),
+        primary_key=True,
+    ),
 )
 
 follow_table = sa.Table(
     "follow_table",
     Base.metadata,
-    sa.Column("follower_id", sa.Integer, sa.ForeignKey("account.id"), primary_key=True),
-    sa.Column("followed_id", sa.Integer, sa.ForeignKey("account.id"), primary_key=True),
+    sa.Column(
+        "follower_id",
+        sa.Integer,
+        sa.ForeignKey("account.id", ondelete="CASCADE"),
+        primary_key=True,
+    ),
+    sa.Column(
+        "followed_id",
+        sa.Integer,
+        sa.ForeignKey("account.id", ondelete="CASCADE"),
+        primary_key=True,
+    ),
 )
 
 
@@ -56,10 +96,10 @@ class Account(Base):
     last_visit: so.Mapped[datetime] = so.mapped_column(sa.DateTime, nullable=True)
 
     reviews: so.Mapped[List["Review"]] = so.relationship(
-        back_populates="author", passive_deletes=True
+        back_populates="author", cascade="all, delete-orphan"
     )
     comments: so.Mapped[List["Comment"]] = so.relationship(
-        back_populates="author", passive_deletes=True
+        back_populates="author", cascade="all, delete-orphan"
     )
     like_reviews: so.Mapped[List["Review"]] = so.relationship(
         "Review", secondary=like_review_table, back_populates="like_accounts"
@@ -102,11 +142,11 @@ class Review(Base):
     )
 
     author_id: so.Mapped[int] = so.mapped_column(
-        sa.ForeignKey("account.id"), nullable=False
+        sa.ForeignKey("account.id", ondelete="CASCADE"), nullable=False
     )
     author: so.Mapped["Account"] = so.relationship(back_populates="reviews")
     comments: so.Mapped[List["Comment"]] = so.relationship(
-        back_populates="review", passive_deletes=True
+        back_populates="review", cascade="all, delete-orphan"
     )
     like_accounts: so.Mapped[List["Account"]] = so.relationship(
         "Account", secondary=like_review_table, back_populates="like_reviews"
@@ -125,11 +165,11 @@ class Comment(Base):
     updated_at: so.Mapped[datetime] = so.mapped_column(sa.DateTime, nullable=True)
 
     review_id: so.Mapped[int] = so.mapped_column(
-        sa.ForeignKey("review.id"), nullable=False
+        sa.ForeignKey("review.id", ondelete="CASCADE"), nullable=False
     )
     review: so.Mapped["Review"] = so.relationship(back_populates="comments")
     author_id: so.Mapped[int] = so.mapped_column(
-        sa.ForeignKey("account.id"), nullable=False
+        sa.ForeignKey("account.id", ondelete="CASCADE"), nullable=False
     )
     author: so.Mapped["Account"] = so.relationship(back_populates="comments")
     like_accounts: so.Mapped[List["Account"]] = so.relationship(
