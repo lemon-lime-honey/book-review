6023b72de7b32f0b9f91966d25ad1e858a796a22
lemon-lime-honey <bs0308@g.skku.edu>
2024-08-05 21:16:42 +0900

    build: :construction_worker: add frontend build
    - 프론트엔드 빌드 설정

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.responses import FileResponse
+from starlette.staticfiles import StaticFiles
 from domain.account import router as account_router
 from domain.comment import router as comment_router
 from domain.review import router as review_router
 
 app = FastAPI()
 
-origins = ["http://localhost:5173", "http://127.0.0.1:5173"]
+origins = ["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:8000"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -19,3 +21,9 @@ app.add_middleware(
 app.include_router(account_router.router)
 app.include_router(review_router.router)
 app.include_router(comment_router.router)
+app.mount("/assets", StaticFiles(directory="../frontend/dist/assets"))
+
+
+@app.get("/")
+def index():
+    return FileResponse("../frontend/dist/index.html")
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+  plugins: [svelte(), topLevelAwait({ promiseExportName: '__tla', promiseImportName: (i) => `__tla_${i}` })],
+});
